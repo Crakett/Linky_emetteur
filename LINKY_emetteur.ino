ad4bf159de1d3a9e4b67f4c35db3d580143450de
Crakett <francois.ste@free.fr>
2023-12-10 14:49:43 +0100

    change radio débit to 250kbs and canal radio to 101

--- LINKY_emetteur.ino
+++ LINKY_emetteur.ino
@@ -4,6 +4,7 @@
 #define pinCE   9                     // On associe la broche "CE" du NRF24L01 à la sortie digitale D7 de l'arduino
 #define pinCSN  10                    // On associe la broche "CSN" du NRF24L01 à la sortie digitale D8 de l'arduino
 #define tunnel  "LINKY"               // On définit un "nom de tunnel" (5 caractères), pour pouvoir communiquer d'un NRF24 à l'autre
+#define canalRadio 101                // defini le canal radio (entre 1 et 125) 101 --> 2,501 GHz
 
 RF24 radio(pinCE, pinCSN);            // Instanciation du NRF24L01
 
@@ -11,10 +12,14 @@ const byte adresse[6] = tunnel;       // Mise au format "byte array" du nom du t
 
 void setup() {
   delay(3000);
+
+  // Initialisation du port série sur 9600 bits/s 7 bits de données parité paire 1 stop
   Serial.begin(9600,SERIAL_7E1);
+
+  // Partie NRF24
   radio.begin();                      // Initialisation du module NRF24
-  radio.setDataRate(RF24_1MBPS);      // en remplaçant « xxx » par RF24_250KBPS, RF24_1MBPS, ou encore, RF24_2MBPS
-  radio.setChannel(100);              // en remplaçant « x » par une valeur comprise entre 0 et 125
+  radio.setDataRate(RF24_250KBPS);      // en remplaçant « xxx » par RF24_250KBPS, RF24_1MBPS, ou encore, RF24_2MBPS
+  radio.setChannel(canalRadio);       // en remplaçant « x » par une valeur comprise entre 0 et 125
   radio.openWritingPipe(adresse);     // Ouverture du tunnel en ÉCRITURE, avec le "nom" qu'on lui a donné
   radio.setPALevel(RF24_PA_HIGH);     // en remplaçant « xxx » par RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, ou RF24_PA_MAX
   radio.stopListening();              // Arrêt de l'écoute du NRF24 (signifiant qu'on va émettre, et non recevoir, ici)
